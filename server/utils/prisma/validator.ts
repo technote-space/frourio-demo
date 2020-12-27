20d546083ca69aebe119a614f83925e454c2bb12
Technote <technote.space@gmail.com>
2020-12-28 00:27:17 +0900

    chore: refine validation error message

--- server/utils/prisma/validator.ts
+++ server/utils/prisma/validator.ts
@@ -42,14 +42,21 @@ export function IsIdExists(table: Models, validationOptions?: ValidationOptions)
 
 @ValidatorConstraint({ async: true })
 class IsReservableConstrains implements ValidatorConstraintInterface {
+  private reason?: string;
+
   async validate(value: any, args: ValidationArguments) {
     const data = args.object as any;
     if (!('roomId' in data) || typeof data['roomId'] !== 'number') {
       return false;
     }
 
-    const checkin     = new Date(data['checkin']);
-    const checkout    = new Date(data['checkout']);
+    const checkin  = new Date(data['checkin']);
+    const checkout = new Date(data['checkout']);
+    if (checkin.getDate() >= checkout.getDate()) {
+      this.reason = 'The checkin time must be before the checkout time.';
+      return false;
+    }
+
     const prisma      = new PrismaClient();
     const reservation = await prisma.reservation.findFirst({
       where: {
@@ -62,12 +69,20 @@ class IsReservableConstrains implements ValidatorConstraintInterface {
         },
       },
     });
+    if (reservation) {
+      this.reason = 'This period is already reserved.';
+      return false;
+    }
 
-    return !reservation && checkin.getDate() < checkout.getDate();
+    return true;
   }
 
   defaultMessage(args: ValidationArguments) {
-    return 'Not Reservable';
+    if (this.reason) {
+      return this.reason;
+    }
+
+    return 'Invalid value';
   }
 }
 
@@ -85,6 +100,8 @@ export function IsReservable(validationOptions?: ValidationOptions) {
 
 @ValidatorConstraint({ async: true })
 class IsWithinLimitConstrains implements ValidatorConstraintInterface {
+  private limit?: number;
+
   async validate(value: any, args: ValidationArguments) {
     if (typeof value !== 'number') {
       return false;
@@ -101,11 +118,20 @@ class IsWithinLimitConstrains implements ValidatorConstraintInterface {
     const prisma    = new PrismaClient();
     const findFirst = prisma[table].findFirst as ((args?: { where: { id: number } }) => Promise<object | null>);
     const item      = await findFirst({ where: { id: Number(data[id]) } });
-    return item !== null && Number(value) <= Number(item[field]);
+    if (!item) {
+      return false;
+    }
+
+    this.limit = Number(item[field]);
+    return item !== null && Number(value) <= this.limit;
   }
 
   defaultMessage(args: ValidationArguments) {
-    return 'Not within the limitation';
+    if (this.limit === undefined) {
+      return 'Invalid value';
+    }
+
+    return 'The value must be less than or equal to ' + this.limit;
   }
 }
 
