cf62b493141441f081e18a5a81a3b19ace36cb36
Technote <technote.space@gmail.com>
2020-12-30 22:49:52 +0900

    chore: refine seeder

--- server/prisma/factories/reservation.ts
+++ server/prisma/factories/reservation.ts
@@ -1,5 +1,7 @@
-import type { ReservationCreateInput, Guest, Room } from '@prisma/client';
+import type { ReservationCreateInput, Room } from '@prisma/client';
+import { isBefore, isAfter } from 'date-fns';
 import { define } from '../tools/define';
+import { ReservationStatus } from '$/types';
 
 define<ReservationCreateInput>('reservation', ((faker, params) => {
   const number  = faker.random.number({ min: 1, max: (params[0] as Room).number });
@@ -9,11 +11,30 @@ define<ReservationCreateInput>('reservation', ((faker, params) => {
   const checkout = new Date(checkin.valueOf());
   checkout.setDate(checkin.getDate() + nights);
   checkout.setHours(10, 0, 0, 0);
+
+  const amount = (params[0] as Room).price * number * nights;
+  let status: ReservationStatus;
+  let payment: number | undefined;
+  const now    = new Date();
+  if (faker.random.number(1000) < 50) {
+    status = 'canceled';
+  } else {
+    if (isBefore(now, checkin)) {
+      status = 'reserved';
+    } else if (isBefore(now, checkout)) {
+      status = 'checkin';
+    } else {
+      status  = 'checkout';
+      payment = amount;
+    }
+  }
+
   return {
     checkin,
     checkout,
-    status: 'reserved',
+    status,
     number,
-    amount: (params[0] as Room).price * number * nights,
+    amount,
+    payment,
   };
 }));
--- server/prisma/factories/room.ts
+++ server/prisma/factories/room.ts
@@ -2,7 +2,7 @@ import type { RoomCreateInput } from '@prisma/client';
 import { define } from '../tools/define';
 
 define<RoomCreateInput>('room', (faker => ({
-  name: faker.name.firstName(),
+  name: faker.name.firstName() + faker.random.number(),
   number: faker.random.number({ min: 1, max: 10 }),
   price: faker.random.number({ min: 1000, max: 100000 }),
 })));
