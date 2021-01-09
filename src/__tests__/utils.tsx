435afcff3bdf4f87df0f586802068960229ebe20
Technote <technote.space@gmail.com>
2021-01-09 14:48:07 +0900

    chore: refine test util

--- src/__tests__/utils.tsx
+++ src/__tests__/utils.tsx
@@ -1,15 +1,40 @@
-import React, { ReactChild } from 'react';
+import type { ReactChild, ReactElement } from 'react';
+import type { RenderOptions } from '@testing-library/react';
 import { render } from '@testing-library/react';
 import { SWRConfig } from 'swr';
+import { CookiesProvider } from 'react-cookie';
+import DateFnsUtils from '@date-io/date-fns';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import { ja } from 'date-fns/locale';
 import { StoreContextProvider } from '~/store';
+import Layout from '~/components/Layout';
 
-const Providers = ({ children }: { children: ReactChild }) => <StoreContextProvider>
-  <SWRConfig value={{ dedupingInterval: 0 }}>{children}</SWRConfig>
-</StoreContextProvider>;
+type ProviderOptions = {
+  wrapLayout?: boolean;
+};
 
-const customRender = (ui, options = {}) => {
+const Providers = (options: ProviderOptions) => ({ children }: { children: ReactChild }) => <CookiesProvider>
+  <StoreContextProvider>
+    <MuiPickersUtilsProvider utils={DateFnsUtils} locale={ja}>
+      <SWRConfig value={{ dedupingInterval: 0 }}>
+        {options.wrapLayout ? <Layout>
+          {children}
+        </Layout> : { children }}
+      </SWRConfig>
+    </MuiPickersUtilsProvider>
+  </StoreContextProvider>
+</CookiesProvider>;
+
+const customRender = (ui: ReactElement, options: ProviderOptions & RenderOptions = {}) => {
+  const { wrapLayout, ...renderOptions } = options;
+
   window.scrollTo = jest.fn();
-  return render(ui, { wrapper: Providers, ...options });
+  return render(ui, {
+    wrapper: Providers({
+      wrapLayout,
+    }),
+    ...renderOptions,
+  });
 };
 
 export * from '@testing-library/react';
