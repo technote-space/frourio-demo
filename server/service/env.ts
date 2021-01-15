d8aaf50d98a40e35dab36e203a0ef37f4f310427
Technote <technote.space@gmail.com>
2021-01-15 23:18:18 +0900

    chore: tweaks

--- server/service/env.ts
+++ server/service/env.ts
@@ -2,9 +2,9 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const JWT_SECRET  = process.env.JWT_SECRET ?? '';
+const JWT_SECRET = process.env.JWT_SECRET ?? '';
 const SERVER_PORT = +(process.env.SERVER_PORT ?? '8080');
-const BASE_PATH   = process.env.BASE_PATH ?? '';
-const API_ORIGIN  = process.env.API_ORIGIN ?? '';
+const BASE_PATH = process.env.BASE_PATH ?? '';
+const API_ORIGIN = process.env.API_ORIGIN ?? '';
 
 export { JWT_SECRET, SERVER_PORT, BASE_PATH, API_ORIGIN };
