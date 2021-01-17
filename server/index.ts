95b247ba60cc7aee2cef1797f970121aab3e73a4
Technote <technote.space@gmail.com>
2021-01-18 08:05:22 +0900

    chore: server log

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,9 @@
 import server from './server';
 import { SERVER_PORT } from './service/env';
 
-server.listen(SERVER_PORT);
+server.listen(SERVER_PORT, (err, address) => {
+  console.log(address);
+  if (err) {
+    console.log(err);
+  }
+});
