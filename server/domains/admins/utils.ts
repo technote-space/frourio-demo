fa57e2e742a83cde837d588f919e6fd55b2562f7
Technote <technote.space@gmail.com>
2021-01-22 01:14:30 +0900

    chore: tweaks

--- server/domains/admins/utils.ts
+++ server/domains/admins/utils.ts
@@ -2,4 +2,4 @@ import { saveFile } from '$/service/multipart';
 import { processRoleConnections } from '$/repositories/admin';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const processBody = async <T extends Record<string, any> & { roles?: string[] }>(body: T) => await saveFile(processRoleConnections(body));
+export const processBody = async <T extends Record<string, any> & { roles?: string[] }>(body: T) => saveFile(processRoleConnections(body));
