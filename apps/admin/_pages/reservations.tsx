f0bb3605f82a3a0a60283e36b465e83787b1d1fc
Technote <technote.space@gmail.com>
2021-02-01 14:20:10 +0900

    fix: consider have no related user and room

--- apps/admin/_pages/reservations.tsx
+++ apps/admin/_pages/reservations.tsx
@@ -33,7 +33,7 @@ const Reservations: FC<AuthenticatedPageProps> = ({ authHeader }: AuthenticatedP
         />,
         // eslint-disable-next-line react/display-name
         render: data => data['guestName'],
-        validate: data => !!data['guestId'],
+        validate: data => !!data['guestName'],
       },
       {
         title: '部屋名',
@@ -46,7 +46,7 @@ const Reservations: FC<AuthenticatedPageProps> = ({ authHeader }: AuthenticatedP
         />,
         // eslint-disable-next-line react/display-name
         render: data => data['roomName'],
-        validate: data => !!data['roomId'],
+        validate: data => !!data['roomName'],
       },
       {
         title: '人数',
