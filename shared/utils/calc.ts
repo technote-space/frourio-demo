9f9a991ee978ced18648e6bbcffb24665185113d
Technote <technote.space@gmail.com>
2021-02-01 14:18:58 +0900

    chore: calc helper

--- shared/utils/calc.ts
+++ shared/utils/calc.ts
@@ -1,11 +1,13 @@
 import { differenceInCalendarDays } from 'date-fns';
 
+export const getNights = (checkin: string | Date, checkout: string | Date): number => differenceInCalendarDays(new Date(checkout), new Date(checkin));
+
 export const getPriceCalc = (price: number, number: number, checkin: string | Date, checkout: string | Date, amount: number): string => {
-  const diff = differenceInCalendarDays(new Date(checkout), new Date(checkin));
-  if (diff <= 0) {
+  const nights = getNights(checkin, checkout);
+  if (nights <= 0) {
     return '';
   }
 
-  const calculated = price * number * diff;
-  return (amount === calculated ? '' : `¥${calculated} = `) + `¥${price} * ${number}人 * ${diff}泊`;
+  const calculated = price * number * nights;
+  return (amount === calculated ? '' : `¥${calculated} = `) + `¥${price} * ${number}人 * ${nights}泊`;
 };
