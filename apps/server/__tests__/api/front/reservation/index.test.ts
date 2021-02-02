8888019f7292fc4c2bb9b6d339bf8fa54fecab45
Technote <technote.space@gmail.com>
2021-02-02 16:32:59 +0900

    chore: change update guest info behavior

--- apps/server/__tests__/api/front/reservation/index.test.ts
+++ apps/server/__tests__/api/front/reservation/index.test.ts
@@ -1,7 +1,7 @@
 import controller from '$/api/front/reservation/controller';
 import { getRoom } from '$/repositories/room';
 import { createReservation } from '$/repositories/reservation';
-import { updateGuest } from '$/repositories/guest';
+import { getGuest, updateGuest } from '$/repositories/guest';
 import { getFastify, getPromiseLikeItem } from '$/__tests__/utils';
 import { reserve } from '$/domains/front/reservation';
 
@@ -86,6 +86,9 @@ describe('reservation', () => {
     const createReservationMock = jest.fn(() => getPromiseLikeItem({
       id: 123,
     }));
+    const getGuestMock = jest.fn(() => getPromiseLikeItem({
+      id: 234,
+    }));
     const updateGuestMock = jest.fn(() => getPromiseLikeItem({
       id: 234,
     }));
@@ -102,6 +105,13 @@ describe('reservation', () => {
           prisma: {
             reservation: {
               create: createReservationMock,
+            },
+          },
+        }),
+        getGuest: getGuest.inject({
+          prisma: {
+            guest: {
+              findFirst: getGuestMock,
             },
           },
         }),
@@ -140,6 +150,19 @@ describe('reservation', () => {
         id: 1,
       },
     });
+    expect(getGuestMock).toBeCalledWith({
+      rejectOnNotFound: true,
+      select: {
+        name: true,
+        nameKana: true,
+        zipCode: true,
+        address: true,
+        phone: true,
+      },
+      where: {
+        id: 321,
+      },
+    });
     expect(createReservationMock).toBeCalledWith({
       data: {
         guest: {
@@ -172,6 +195,132 @@ describe('reservation', () => {
         zipCode: '100-0001',
         address: 'テスト県テスト市',
         phone: '03-0000-0000',
+      },
+      where: {
+        id: 321,
+      },
+    });
+  });
+
+  it('should create reservation with update info', async() => {
+    const getRoomMock = jest.fn(() => getPromiseLikeItem({
+      name: 'room name',
+      price: 10000,
+    }));
+    const createReservationMock = jest.fn(() => getPromiseLikeItem({
+      id: 123,
+    }));
+    const getGuestMock = jest.fn(() => getPromiseLikeItem({
+      id: 234,
+      name: 'guest name',
+      nameKana: 'テスト',
+      zipCode: '100-0001',
+    }));
+    const updateGuestMock = jest.fn(() => getPromiseLikeItem({
+      id: 234,
+    }));
+    const injectedController = controller.inject({
+      reserve: reserve.inject({
+        getRoom: getRoom.inject({
+          prisma: {
+            room: {
+              findFirst: getRoomMock,
+            },
+          },
+        }),
+        createReservation: createReservation.inject({
+          prisma: {
+            reservation: {
+              create: createReservationMock,
+            },
+          },
+        }),
+        getGuest: getGuest.inject({
+          prisma: {
+            guest: {
+              findFirst: getGuestMock,
+            },
+          },
+        }),
+        updateGuest: updateGuest.inject({
+          prisma: {
+            guest: {
+              update: updateGuestMock,
+            },
+          },
+        }),
+      }),
+    })(getFastify());
+
+    const res = await injectedController.post({
+      body: {
+        guestName: 'guest name 2',
+        guestNameKana: 'テスト 2',
+        guestZipCode: '100-0002',
+        guestAddress: 'テスト県テスト市2',
+        guestPhone: '03-0000-0002',
+        roomId: 1,
+        number: 2,
+        checkin: '2020-01-01',
+        checkout: '2020-01-03',
+      },
+      user: { id: 321 },
+      headers: undefined,
+    });
+    expect(res.body).toEqual({
+      id: 123,
+    });
+    expect(getRoomMock).toBeCalledWith({
+      rejectOnNotFound: true,
+      where: {
+        id: 1,
+      },
+    });
+    expect(getGuestMock).toBeCalledWith({
+      rejectOnNotFound: true,
+      select: {
+        name: true,
+        nameKana: true,
+        zipCode: true,
+        address: true,
+        phone: true,
+      },
+      where: {
+        id: 321,
+      },
+    });
+    expect(createReservationMock).toBeCalledWith({
+      data: {
+        guest: {
+          connect: {
+            id: 321,
+          },
+        },
+        guestName: 'guest name 2',
+        guestNameKana: 'テスト 2',
+        guestZipCode: '100-0002',
+        guestAddress: 'テスト県テスト市2',
+        guestPhone: '03-0000-0002',
+        number: 2,
+        checkin: new Date('2020-01-01'),
+        checkout: new Date('2020-01-03'),
+        room: {
+          connect: {
+            id: 1,
+          },
+        },
+        roomName: 'room name',
+        amount: 40000,
+        status: 'reserved',
+      },
+    });
+    expect(updateGuestMock).toBeCalledWith({
+      data: {
+        name: 'guest name',
+        nameKana: 'テスト',
+        zipCode: '100-0001',
+        address: 'テスト県テスト市2',
+        phone: '03-0000-0002',
       },
       where: {
         id: 321,
