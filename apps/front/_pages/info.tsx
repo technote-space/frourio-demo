033c1980e8b1fb882814e40c4791767d6572a667
Technote <technote.space@gmail.com>
2021-02-03 03:56:25 +0900

    chore: tweaks

--- apps/front/_pages/info.tsx
+++ apps/front/_pages/info.tsx
@@ -1,12 +1,13 @@
 import type { FC } from 'react';
 import { memo } from 'react';
-import { Icon } from '@chakra-ui/react';
+import { Box, Heading, Icon } from '@chakra-ui/react';
 import { IoMdConstruct } from 'react-icons/io';
 
 const Info: FC = memo(() => {
-  return <div>
+  return <Box m={4}>
+    <Heading m={2}>お知らせ</Heading>
     <Icon as={IoMdConstruct}/> 工事中...
-  </div>;
+  </Box>;
 });
 
 Info.displayName = 'Info';
