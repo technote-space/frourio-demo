8648737b6e5b0137308c6ba64dbb56ea022b3e6b
Technote <technote.space@gmail.com>
2021-02-02 17:24:11 +0900

    feat: create pid file (#66)

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -1,15 +1,35 @@
 import { promisify } from 'util';
 import { exec } from 'child_process';
+import { promises, existsSync } from 'fs';
 import server from './server';
 import { SERVER_PORT, SERVER_ADDRESS } from './service/env';
 
 (async() => {
   if (process.platform !== 'win32') {
     const execAsync = promisify(exec);
-    const pid = (await execAsync(`lsof -i:${SERVER_PORT} -t || :`)).stdout;
-    if (pid) {
-      await execAsync(`kill -9 ${pid}`);
-    }
+    const pidFile = 'server.pid';
+    const findPidByFile = async() => {
+      if (existsSync(pidFile)) {
+        return (await promises.readFile(pidFile)).toString();
+      }
+
+      return '';
+    };
+    const createPidFile = async() => {
+      console.log('pid:', process.pid);
+      await promises.writeFile(pidFile, `${process.pid}`);
+    };
+    const findPidByPort = async() => (await execAsync(`lsof -i:${SERVER_PORT} -t || :`)).stdout;
+    const kill = async(pid: string) => {
+      if (pid) {
+        console.log('kill:', pid);
+        await execAsync(`kill -9 ${pid} || :`);
+      }
+    };
+
+    await kill(await findPidByFile());
+    await kill(await findPidByPort());
+    await createPidFile();
   }
 })().then(() => {
   server.listen(SERVER_PORT, SERVER_ADDRESS, (err, address) => {
