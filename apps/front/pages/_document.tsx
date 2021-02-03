8a0c8c499f3ced756700bf806657cb155b4775ce
Technote <technote.space@gmail.com>
2021-02-03 14:01:12 +0900

    chore: add html comment to license file

--- apps/front/pages/_document.tsx
+++ apps/front/pages/_document.tsx
@@ -1,17 +1,18 @@
 import { ColorModeScript } from '@chakra-ui/react';
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
 
+const getLicenseUrl = () => `${process.env.BASE_PATH}/license.json`;
 export default class Document extends NextDocument {
   render() {
-    return (
-      <Html lang="ja">
-        <Head/>
-        <body>
-          <ColorModeScript initialColorMode="system"/>
-          <Main/>
-          <NextScript/>
-        </body>
-      </Html>
-    );
+    return <Html lang="ja">
+      <head dangerouslySetInnerHTML={{ __html: `<!-- Licenses: ${getLicenseUrl()} -->` }}>
+      </head>
+      <Head/>
+      <body>
+        <ColorModeScript initialColorMode="system"/>
+        <Main/>
+        <NextScript/>
+      </body>
+    </Html>;
   }
 }
