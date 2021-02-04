354c8f8ccea67d7ba9638345447bed6d39d24685
Technote <technote.space@gmail.com>
2021-02-04 21:57:59 +0900

    chore: tweaks

--- apps/server/prisma/factories/guest.ts
+++ apps/server/prisma/factories/guest.ts
@@ -3,7 +3,7 @@ import { define } from '../tools/define';
 define('guest', (faker => ({
   email: faker.random.number() + faker.internet.email(),
   name: `${faker.name.lastName()} ${faker.name.firstName()}`,
-  nameKana: `${faker.name.lastName()} ${faker.name.firstName()}`,
+  nameKana: 'テスト テスト',
   zipCode: faker.address.zipCode(),
   address: `${faker.address.country()} ${faker.address.city()} ${faker.address.streetName()}`,
   phone: faker.phone.phoneNumber(),
