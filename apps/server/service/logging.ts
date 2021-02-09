8477259fe832927a161ff206f5b26ed02937e91f
Technote <technote.space@gmail.com>
2021-02-10 03:51:46 +0900

    chore: add log helper

---
+++ apps/server/service/logging.ts
@@ -0,0 +1,17 @@
+import { createLogger } from 'bunyan';
+import { resolve } from 'path';
+
+console.log(process.env.NODE_ENV === 'development' ? /* istanbul ignore next */ 'debug' : 'warn');
+export const logger = createLogger({
+  name: 'system',
+  streams: [{
+    type: 'rotating-file',
+    path: resolve(process.cwd(), 'logs', 'system.log'),
+    period: '1d',
+    count: 10,
+  }],
+  src: true,
+  level: process.env.NODE_ENV === 'development' ? /* istanbul ignore next */ 'debug' : 'warn',
+});
+
+export default logger;
