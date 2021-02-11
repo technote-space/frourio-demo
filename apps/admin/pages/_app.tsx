f3516a6744695ef4ee4ee1f20e8392247f9bc374
Technote <technote.space@gmail.com>
2021-02-12 01:49:59 +0900

    chore: revert

--- apps/admin/pages/_app.tsx
+++ apps/admin/pages/_app.tsx
@@ -1,14 +1,23 @@
-import type { PropsWithChildren } from 'react';
+import type { FC, PropsWithChildren } from 'react';
 import type { AppProps } from 'next/app';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Head from '~/components/Head';
 import { StoreContextProvider } from '~/store';
 
-const MyApp = ({ Component, pageProps }: PropsWithChildren<AppProps>) => typeof window === 'undefined' ? null :
+const SafeHydrate: FC = ({ children }: PropsWithChildren<{}>) => {
+  return (
+    <div suppressHydrationWarning>
+      {typeof window === 'undefined' ? null : children}
+    </div>
+  );
+};
+
+const MyApp = ({ Component, pageProps }: PropsWithChildren<AppProps>) => <SafeHydrate>
   <StoreContextProvider>
     <Head />
     <CssBaseline />
     <Component {...pageProps} />
-  </StoreContextProvider>;
+  </StoreContextProvider>
+</SafeHydrate>;
 
 export default MyApp;
