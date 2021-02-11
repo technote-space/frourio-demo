14aa529de1a0367491550ab46c7dcdef39409228
Technote <technote.space@gmail.com>
2021-02-11 20:53:10 +0900

    chore: not send test address

--- apps/server/service/mail/task.ts
+++ apps/server/service/mail/task.ts
@@ -1,8 +1,24 @@
-import type { MailOptions, MailSettings } from '$/types';
+import type { MailAddress, MailOptions, MailSettings } from '$/types';
 import { createTransport } from 'nodemailer';
 import { logger } from '$/service/logging';
 
+const isTestAddress = (to: MailAddress): boolean => {
+  if (Array.isArray(to)) {
+    return to.some(to => isTestAddress(to));
+  }
+
+  if (typeof to === 'object') {
+    return isTestAddress(to.address);
+  }
+
+  return /example\.com/.test(to);
+};
 process.once('message', (message: { options: MailOptions; settings: MailSettings; }) => {
+  if (isTestAddress(message.settings.to)) {
+    logger.debug(`test address: ${JSON.stringify(message.settings.to)}`);
+    return;
+  }
+
   const transport = createTransport(message.options);
   transport.sendMail(message.settings, (error, result) => {
     if (error) {
