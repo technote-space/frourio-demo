46e8ef2a574b6e7591df444b3b3757d709084656
Technote <technote.space@gmail.com>
2021-02-14 00:47:46 +0900

    chore: remove extra data

--- apps/server/utils/reservation.ts
+++ apps/server/utils/reservation.ts
@@ -7,21 +7,22 @@ import { ROOM_KEY_DIGITS } from '@frourio-demo/constants';
 const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567'.split('');
 export const generateCode = (): string => randomBytes(16).reduce((acc, value) => acc + chars[(value % 32)], '');
 export const generateRoomKey = (): string => `${'0'.repeat(ROOM_KEY_DIGITS)}${randomInt(0, Math.pow(10, ROOM_KEY_DIGITS))}`.slice(-ROOM_KEY_DIGITS);
+const bufferToArray = (buffer: Buffer) => JSON.parse(JSON.stringify(buffer)).data;
 export const encryptQrInfo = (info: QrInfo): string => {
   const key = scryptSync(CRYPTO_PASS, CRYPTO_SALT, 32);
   const iv = randomBytes(16);
   const cipher = createCipheriv(CRYPTO_ALGO, key, iv);
   return Buffer.from(JSON.stringify({
-    data: Buffer.concat([cipher.update(JSON.stringify(info)), cipher.final()]),
-    iv,
+    data: bufferToArray(Buffer.concat([cipher.update(JSON.stringify(info)), cipher.final()])),
+    iv: bufferToArray(iv),
   })).toString('base64');
 };
 export const decryptQrInfo = (encrypted: string): QrInfo | undefined => {
   try {
     const { data, iv } = JSON.parse(Buffer.from(encrypted, 'base64').toString());
     const key = scryptSync(CRYPTO_PASS, CRYPTO_SALT, 32);
-    const decipher = createDecipheriv(CRYPTO_ALGO, key, Buffer.from(iv.data));
-    return JSON.parse(Buffer.concat([decipher.update(Buffer.from(data.data)), decipher.final()]).toString()) as QrInfo;
+    const decipher = createDecipheriv(CRYPTO_ALGO, key, Buffer.from(iv));
+    return JSON.parse(Buffer.concat([decipher.update(Buffer.from(data)), decipher.final()]).toString()) as QrInfo;
   } catch (error) {
     logger.error(error);
     return undefined;
