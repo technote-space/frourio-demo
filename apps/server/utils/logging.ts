9389c8531d9a432f5b702e3df8e95850238ff8b5
Technote <technote.space@gmail.com>
2021-02-18 21:21:15 +0900

    fix: logger

--- apps/server/utils/logging.ts
+++ apps/server/utils/logging.ts
@@ -1,4 +1,3 @@
-import type Logger from 'bunyan';
 import { createLogger } from 'bunyan';
 import { resolve } from 'path';
 import { mkdirSync } from 'fs';
@@ -7,7 +6,7 @@ import { sendError } from '$/utils/slack';
 const dir = resolve(process.cwd(), 'logs');
 mkdirSync(dir, { recursive: true });
 
-const _logger = createLogger({
+const logger = createLogger({
   name: 'system',
   streams: [{
     type: 'rotating-file',
@@ -18,16 +17,17 @@ const _logger = createLogger({
   src: true,
   level: process.env.NODE_ENV === 'development' ? /* istanbul ignore next */ 'debug' : 'info',
 });
+const errorLog = logger.error;
+
 /* istanbul ignore next */
 const _error = (value?: any) => { // eslint-disable-line @typescript-eslint/no-explicit-any
-  _logger.error(value);
+  errorLog(value);
   if (value instanceof Error) {
     sendError(value);
   }
-};
-export const logger = {
-  ..._logger,
-  error: _error,
-} as Logger;
 
-export default logger;
+  return true;
+};
+logger.error = _error;
+
+export { logger };
